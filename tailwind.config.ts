b7a69e2f20806ffd441ca4009a51215afa103633
ZikQ-Shmily <zikq.shmily@gmail.com>
2025-08-26 01:39:07 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ const config: Config = {
   darkMode: ["class"],
   content: [
     "./index.html",
-    "./src/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}"
+    "./src/**/*.{ts,tsx,js,jsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/tools/**/*.{ts,tsx}"
   ],
   theme: {
     container: {
@@ -56,8 +57,14 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)"
       },
       keyframes: {
-        "accordion-down": { from: { height: "0" }, to: { height: "var(--radix-accordion-content-height)" } },
-        "accordion-up": { from: { height: "var(--radix-accordion-content-height)" }, to: { height: "0" } }
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" }
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
